d2a8312fbc854d20e33d6d503446591c1312b200
Alberto ammar <albertoammarw@gmail.com>
2020-10-02 01:11:05 -0300

    chore: configure root import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { B } from '~/components/b';
 
 function App(): JSX.Element {
-    return <div className="App" />;
+    return (
+        <div className="App">
+            Hello
+            <B />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/b.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function B(): JSX.Element {
+   return <div className="App">Hello</div>;
+}

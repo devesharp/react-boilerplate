93e273f493672b44bbc24f6e1556c907e827ff1b
Alberto Ammar <albertoammarw@gmail.com>
2021-04-08 17:47:31 -0300

    style: update lint with defaultTheme styled component

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,6 +1,15 @@
 /// <reference types="react-scripts" />
+// import original module declarations
+import 'styled-components';
+import {ITheme} from "@devesharp/react-web";
 
 // eslint-disable-next-line @typescript-eslint/interface-name-prefix
 interface Console {
    tron: any;
+}
+
+// and extend them!
+declare module 'styled-components' {
+   export interface DefaultTheme extends ITheme{
+   }
 }

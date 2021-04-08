52eaa6c0ed5ba0cd61ef69994b226a55145e8bd2
Alberto Ammar <albertoammarw@gmail.com>
2021-04-08 17:48:55 -0300

    feat: update template to use Page in name

--- templates/pages/TemplateNamePage.tsx
+++ templates/pages/TemplateNamePage.tsx
@@ -3,7 +3,7 @@ import { ITemplateNameProps } from './TemplateNamePage.interface';
 import { TemplateNamePageController } from './TemplateNamePage.controller';
 import * as S from './TemplateNamePage.style';
 
-export const TemplateName: FunctionComponent<ITemplateNameProps> = function TemplateName(props) {
+export const TemplateNamePage: FunctionComponent<ITemplateNameProps> = function TemplateName(props) {
    const ctrl = TemplateNamePageController();
 
    return <S.Container>{/**/}</S.Container>;
--- templates/pages/index.ts
+++ templates/pages/index.ts
@@ -1,2 +1,2 @@
-export * from './TemplateName';
+export * from './TemplateNamePage';
 export * from './TemplateNamePage.interface';

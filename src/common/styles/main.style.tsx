eb5c1e35864657c34ce52008d6a301e54c640437
Alberto Ammar <albertoammarw@gmail.com>
2021-04-09 08:16:40 -0300

    feat: update input autocomplete chrome

--- src/common/styles/main.style.tsx
+++ src/common/styles/main.style.tsx
@@ -10,6 +10,13 @@ export default css<any>`
       outline: 0;
    }
 
+   input:-webkit-autofill,
+   input:-webkit-autofill:hover,
+   input:-webkit-autofill:focus,
+   input:-webkit-autofill:active {
+      transition: background-color 5000s ease-in-out 0s;
+   }
+
    html,
    body,
    #root {

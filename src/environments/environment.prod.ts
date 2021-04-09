6e86e1b2957f53d98ca9d1f8d88efa6c0b19b1a4
Alberto Ammar <albertoammarw@gmail.com>
2021-04-09 11:16:51 -0300

    feat: add default enviroments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+import { IEnv } from './environment.interface';
+
+export const environment: IEnv = {
+   production: false,
+   API: 'https://localhost:8000/api',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+import { IEnv } from '~/environments/environment.interface';
+
+export const environment: IEnv = {
+   production: false,
+   API: 'https://localhost:8000/api',
+};

8425076bf35accb460a6cf4f9802085de079c780
Alberto Ammar <albertoammarw@gmail.com>
2021-04-09 12:08:11 -0300

    chore: update webpack to production

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,20 @@
 const { addBabelPlugin, override } = require('customize-cra');
+const argv = require('yargs-parser')(process.argv.slice(2));
 
-module.exports = override(
-    addBabelPlugin([
-        'babel-plugin-root-import',
-        {
+module.exports = function overrideConfig(config, env) {
+   /**
+    * Trocar enviroments para prod
+    */
+   if (argv.prod || env === 'production') {
+      config.resolve.extensions = ['.prod.ts', ...config.resolve.extensions];
+   }
+
+   return override(
+      addBabelPlugin([
+         'babel-plugin-root-import',
+         {
             rootPathSuffix: 'src',
-        },
-    ]),
-);
+         },
+      ]),
+   )(config, env);
+};

13ef6099ae60c18c1e9ca24c68ae4d76148e5a6d
Alberto Ammar <albertoammarw@gmail.com>
2021-04-11 13:00:01 -0300

    feat: add theme header

--- src/app/common/config/theme.ts
+++ src/app/common/config/theme.ts
@@ -3,4 +3,9 @@ import { ITheme, ThemeDefault } from '@devesharp/react-web';
 export const ThemeVariables: ITheme = {
    ...ThemeDefault,
    primary: '#09abed',
+   header: {
+      backgroundColor: 'white',
+      textColor: '#444',
+      textColorMenu: '#444',
+   },
 };
